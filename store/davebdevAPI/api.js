c8930b2723b078d6571cb9e564da3ba0ba880ce1
Crowdbotics <engineering@crowdbotics.com>
2023-07-13 14:44:35 +0000

    Update store.

--- store/davebdevAPI/api.js
+++ store/davebdevAPI/api.js
@@ -1,72 +1,52 @@
-import axios from "axios";
+import axios from "axios"
 const davebdevAPI = axios.create({
   baseURL: "https://dave20230706b-dev-98397.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return davebdevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return davebdevAPI.post(`/api/v1/login/`, payload.data);
+  return davebdevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return davebdevAPI.post(`/api/v1/signup/`, payload.data);
+  return davebdevAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return davebdevAPI.post(`/rest-auth/login/`, payload.data);
+  return davebdevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return davebdevAPI.get(`/rest-auth/logout/`);
+  return davebdevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return davebdevAPI.post(`/rest-auth/logout/`);
+  return davebdevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return davebdevAPI.post(`/rest-auth/password/change/`, payload.data);
+  return davebdevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return davebdevAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return davebdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return davebdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return davebdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return davebdevAPI.post(`/rest-auth/registration/`, payload.data);
+  return davebdevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return davebdevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return davebdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return davebdevAPI.get(`/rest-auth/user/`);
+  return davebdevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return davebdevAPI.put(`/rest-auth/user/`, payload.data);
+  return davebdevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return davebdevAPI.patch(`/rest-auth/user/`, payload.data);
+  return davebdevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +62,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
